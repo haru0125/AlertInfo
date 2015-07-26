254a36acbad2b2fb17cb7613d0d1505d17e75882
harute <blurry_alpha@hotmail.com>
2015-07-27 00:29:16 +0900

    mac commit

--- app/src/main/java/jp/co/devhogata/alertinfo/AlertInfo.java
+++ app/src/main/java/jp/co/devhogata/alertinfo/AlertInfo.java
@@ -1,20 +1,34 @@
 package jp.co.devhogata.alertinfo;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class AlertInfo extends ActionBarActivity {
+
+    // クラス変数
+    private ActionBarDrawerToggle mDrawerToggle = null;
+    private DrawerLayout mDrawerLayout = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert_info);
 
-        Log.i("test", "test Log");
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            // ナビゲーションドロワーオープン
+            @Override public void onDrawerOpened(View drawerView) {
+
+            }
+
+        };
     }
 
     @Override
