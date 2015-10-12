0d83bf42a6edecc772624cf42d1445d5132077dd
harute <blurry_alpha@hotmail.com>
2015-10-12 23:57:05 +0900

    キーボードをクローズする

--- app/src/main/java/jp/co/devhogata/alertinfo/AlertInfo.java
+++ app/src/main/java/jp/co/devhogata/alertinfo/AlertInfo.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,6 +37,8 @@ public class AlertInfo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // キーボードをクローズする
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         setContentView(R.layout.activity_alert_info);
 
         // nend
